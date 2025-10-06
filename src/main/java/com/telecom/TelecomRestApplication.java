10e827cd01887e53a44d326d69ff2ab94b21cbf0
gtapiero <giovanny.tapiero@pi.edu.co>
2025-10-06 11:06:40 -0500

    commit - initial :.

---
+++ src/main/java/com/telecom/TelecomRestApplication.java
@@ -0,0 +1,13 @@
+package com.telecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelecomRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelecomRestApplication.class, args);
+    }
+
+}
